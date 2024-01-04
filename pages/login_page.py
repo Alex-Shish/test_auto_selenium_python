96b649994c211c2439b6cd926b21b1d3223f0dff
Alex-Shish <alexshishg@gmail.com>
2024-01-04 21:43:30 +0300

    change login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,24 +1,24 @@
 from .base_page import BasePage
-from .locators import LoginPageLocators
+from .locators import MainPageLocators, LoginPageLocators
 
 url = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
 
 
-def should_be_login_url():
-    login_link = BasePage.open().find_element(*LoginPageLocators.LOGIN_LINK)
-    login_link.click()
-
-
 class LoginPage(BasePage):
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
 
+
+    def should_be_login_url(self):
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_EMAIL), "Login link is not presented"
+
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_REG_EMAIL), "Login link is not presented"
+
