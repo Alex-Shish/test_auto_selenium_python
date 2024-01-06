29c872775a9eeb6ed90f93365955839df578e1a8
Alex-Shish <alexshishg@gmail.com>
2024-01-06 12:32:31 +0300

    change test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,29 @@
 from .pages.main_page import MainPage
-
-link = "http://selenium1py.pythonanywhere.com/"
+from .pages.login_page import LoginPage
+from .pages.product_page import ProductPage
+from .pages.locators import BasketPageLocators
 
 def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
+
 
 def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.should_be_login_url()
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_control_url()
+
+def test_guest_add_product_to_basket(browser):
+    add_btn = browser.find_element(BasketPageLocators, "ADD_BASKET")
+    add_btn.click()
+    x = ProductPage.solve_quiz_and_get_code()
